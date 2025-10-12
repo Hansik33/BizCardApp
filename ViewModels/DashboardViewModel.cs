5c36f21f5b6b9c973dcb46b9df86afbdcf2af5d9
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-10-12 18:00:48 +0200

    feat(dashboard): add save/add/delete commands

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace BizCardApp.ViewModels
-{
-    public partial class DashboardViewModel : BaseViewModel
-    {
+﻿using BizCardApp.Helpers;
+using System.Diagnostics;
+using System.Windows.Input;
 
+namespace BizCardApp.ViewModels;
+
+public partial class DashboardViewModel : BaseViewModel
+{
+    public ICommand SaveChangesCommand { get; }
+    public ICommand AddBusinessCardCommand { get; }
+    public ICommand DeleteBusinessCardCommand { get; }
+
+    public DashboardViewModel()
+    {
+        SaveChangesCommand = new RelayCommand(SaveChanges);
+        AddBusinessCardCommand = new RelayCommand(AddBusinessCard);
+        DeleteBusinessCardCommand = new RelayCommand(DeleteBusinessCard);
     }
+
+    private void SaveChanges() => Debug.WriteLine("SaveChanges");
+
+    private void AddBusinessCard() => Debug.WriteLine("AddBusinessCard");
+
+    private void DeleteBusinessCard() => Debug.WriteLine("DeleteBusinessCard");
 }
