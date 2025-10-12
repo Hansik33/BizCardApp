e689f0e56657fea82332168405f3cb5df226d028
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-10-12 22:52:54 +0200

    feat(dashboard): form bindings + CRUD

--- ViewModels/BusinessCardViewModel.cs
+++ ViewModels/BusinessCardViewModel.cs
@@ -2,5 +2,62 @@
 
 public partial class BusinessCardViewModel : BaseViewModel
 {
+    public string FullName => $"{FirstName} {LastName}".Trim();
 
+    private string _firstName = string.Empty;
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            if (SetProperty(ref _firstName, value))
+                OnPropertyChanged(nameof(FullName));
+        }
+    }
+
+    private string _lastName = string.Empty;
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            if (SetProperty(ref _lastName, value))
+                OnPropertyChanged(nameof(FullName));
+        }
+    }
+
+    private string? _company;
+    public string? Company
+    {
+        get => _company;
+        set => SetProperty(ref _company, value);
+    }
+
+    private string? _jobTitle;
+    public string? JobTitle
+    {
+        get => _jobTitle;
+        set => SetProperty(ref _jobTitle, value);
+    }
+
+    private string? _phone;
+    public string? Phone
+    {
+        get => _phone;
+        set => SetProperty(ref _phone, value);
+    }
+
+    private string? _email;
+    public string? Email
+    {
+        get => _email;
+        set => SetProperty(ref _email, value);
+    }
+
+    private string? _address;
+    public string? Address
+    {
+        get => _address;
+        set => SetProperty(ref _address, value);
+    }
 }
--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using BizCardApp.Helpers;
+using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows.Input;
 
@@ -6,20 +8,95 @@ namespace BizCardApp.ViewModels;
 
 public partial class DashboardViewModel : BaseViewModel
 {
+    public ObservableCollection<BusinessCardViewModel> BusinessCards { get; } = [];
+
+    public BusinessCardViewModel BusinessCardForm => SelectedBusinessCard ?? NewBusinessCardDraft;
+
+    private BusinessCardViewModel _newBusinessCardDraft = new();
+    private BusinessCardViewModel NewBusinessCardDraft
+    {
+        get => _newBusinessCardDraft;
+        set
+        {
+            if (SetProperty(ref _newBusinessCardDraft, value))
+                OnPropertyChanged(nameof(BusinessCardForm));
+        }
+    }
+
+    private BusinessCardViewModel? _selectedBusinessCard;
+    public BusinessCardViewModel? SelectedBusinessCard
+    {
+        get => _selectedBusinessCard;
+        set
+        {
+            if (SetProperty(ref _selectedBusinessCard, value))
+            {
+                OnPropertyChanged(nameof(BusinessCardForm));
+                (SaveChangesCommand as RelayCommand)?.RaiseCanExecuteChanged();
+                (DeleteBusinessCardCommand as RelayCommand)?.RaiseCanExecuteChanged();
+            }
+        }
+    }
+
     public ICommand SaveChangesCommand { get; }
     public ICommand AddBusinessCardCommand { get; }
     public ICommand DeleteBusinessCardCommand { get; }
 
     public DashboardViewModel()
     {
-        SaveChangesCommand = new RelayCommand(SaveChanges);
+        SaveChangesCommand = new RelayCommand(SaveChanges, () => SelectedBusinessCard is not null);
         AddBusinessCardCommand = new RelayCommand(AddBusinessCard);
-        DeleteBusinessCardCommand = new RelayCommand(DeleteBusinessCard);
+        DeleteBusinessCardCommand = new RelayCommand(DeleteBusinessCard, () => SelectedBusinessCard is not null);
     }
 
-    private void SaveChanges() => Debug.WriteLine("SaveChanges");
+    private void SaveChanges()
+    {
+        if (SelectedBusinessCard != null)
+        {
+            Debug.WriteLine($"Godność: {SelectedBusinessCard.FirstName} {SelectedBusinessCard.LastName}\r\n" +
+                $"Firma: {SelectedBusinessCard.Company}\r\n" +
+                $"Stanowisko: {SelectedBusinessCard.JobTitle}\r\n" +
+                $"Telefon: {SelectedBusinessCard.Phone}\r\n" +
+                $"E-mail: {SelectedBusinessCard.Email}\r\n" +
+                $"Adres: {SelectedBusinessCard.Address}");
+        }
+    }
 
-    private void AddBusinessCard() => Debug.WriteLine("AddBusinessCard");
+    private void AddBusinessCard()
+    {
+        var businessCardForm = BusinessCardForm;
 
-    private void DeleteBusinessCard() => Debug.WriteLine("DeleteBusinessCard");
+        if (string.IsNullOrWhiteSpace(businessCardForm.FirstName) || string.IsNullOrWhiteSpace(businessCardForm.LastName))
+            return;
+
+        if (SelectedBusinessCard is null)
+        {
+            var businessCard = _newBusinessCardDraft;
+            BusinessCards.Add(businessCard);
+            SelectedBusinessCard = businessCard;
+
+            _newBusinessCardDraft = new BusinessCardViewModel();
+
+            OnPropertyChanged(nameof(BusinessCardForm));
+        }
+        else
+        {
+            var businessCard = new BusinessCardViewModel();
+            BusinessCards.Add(businessCard);
+            SelectedBusinessCard = businessCard;
+        }
+    }
+
+    private void DeleteBusinessCard()
+    {
+        if (SelectedBusinessCard is null) return;
+
+        var index = BusinessCards.IndexOf(SelectedBusinessCard);
+
+        BusinessCards.Remove(SelectedBusinessCard);
+
+        SelectedBusinessCard = BusinessCards.Count == 0
+            ? null
+            : BusinessCards[Math.Clamp(index - 1, 0, BusinessCards.Count - 1)];
+    }
 }
