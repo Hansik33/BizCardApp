076d2e48d08667519e4044fc99fe8828168f8d0a
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2025-12-02 01:04:35 +0100

    fix(services): show db error dialog

--- Services/BusinessCardService.cs
+++ Services/BusinessCardService.cs
@@ -1,14 +1,17 @@
 ﻿using BizCardApp.Data;
 using BizCardApp.Interfaces;
 using BizCardApp.Models;
+using BizCardApp.Resources;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace BizCardApp.Services;
 
-public sealed class BusinessCardService(IDbContextFactory<AppDbContext> factory) : IBusinessCardService
+public sealed class BusinessCardService(IDbContextFactory<AppDbContext> factory, IDialogService dialogService) : IBusinessCardService
 {
     public async Task<bool> CanConnectAsync(CancellationToken cancellationToken = default)
     {
@@ -18,60 +21,87 @@ public sealed class BusinessCardService(IDbContextFactory<AppDbContext> factory)
 
     public async Task<IReadOnlyList<BusinessCard>> GetAllBusinessCardsAsync(CancellationToken cancellationToken = default)
     {
-        await using var appDbContext = await factory.CreateDbContextAsync(cancellationToken);
+        try
+        {
+            await using var appDbContext = await factory.CreateDbContextAsync(cancellationToken);
 
-        return await appDbContext.BusinessCards
-            .AsNoTracking()
-            .ToListAsync(cancellationToken);
+            return await appDbContext.BusinessCards
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+        catch
+        {
+            await dialogService.ShowMessageAsync(AppStrings.Dialogs.UnableToConnectDatabase, Enums.DialogType.Error);
+            Process.GetCurrentProcess().Kill();
+            return [];
+        }
     }
 
     public async Task<BusinessCard?> SaveBusinessCardAsync(BusinessCard businessCard, CancellationToken cancellationToken = default)
     {
-        await using var appDbContext = await factory.CreateDbContextAsync(cancellationToken);
-
-        if (businessCard.Id == 0)
+        try
         {
-            var entityEntry = await appDbContext.BusinessCards.AddAsync(businessCard, cancellationToken);
+            await using var appDbContext = await factory.CreateDbContextAsync(cancellationToken);
 
-            await appDbContext.SaveChangesAsync(cancellationToken);
+            if (businessCard.Id == 0)
+            {
+                var entityEntry = await appDbContext.BusinessCards.AddAsync(businessCard, cancellationToken);
 
-            return entityEntry.Entity;
+                await appDbContext.SaveChangesAsync(cancellationToken);
+
+                return entityEntry.Entity;
+            }
+            else
+            {
+                var existing = await appDbContext.BusinessCards
+                    .FirstOrDefaultAsync(card => card.Id == businessCard.Id, cancellationToken);
+
+                if (existing is null)
+                    return null;
+
+                existing.FirstName = businessCard.FirstName;
+                existing.LastName = businessCard.LastName;
+                existing.Company = businessCard.Company;
+                existing.JobTitle = businessCard.JobTitle;
+                existing.Phone = businessCard.Phone;
+                existing.Email = businessCard.Email;
+                existing.Address = businessCard.Address;
+
+                await appDbContext.SaveChangesAsync(cancellationToken);
+
+                return existing;
+            }
         }
-        else
+        catch
         {
-            var existing = await appDbContext.BusinessCards
-                .FirstOrDefaultAsync(card => card.Id == businessCard.Id, cancellationToken);
-
-            if (existing is null)
-                return null;
-
-            existing.FirstName = businessCard.FirstName;
-            existing.LastName = businessCard.LastName;
-            existing.Company = businessCard.Company;
-            existing.JobTitle = businessCard.JobTitle;
-            existing.Phone = businessCard.Phone;
-            existing.Email = businessCard.Email;
-            existing.Address = businessCard.Address;
-
-            await appDbContext.SaveChangesAsync(cancellationToken);
-
-            return existing;
+            await dialogService.ShowMessageAsync(AppStrings.Dialogs.UnableToConnectDatabase, Enums.DialogType.Error);
+            Process.GetCurrentProcess().Kill();
+            return null;
         }
     }
 
     public async Task<bool> DeleteBusinessCardAsync(int id, CancellationToken cancellationToken = default)
     {
-        await using var appDbContext = await factory.CreateDbContextAsync(cancellationToken);
+        try
+        {
+            await using var appDbContext = await factory.CreateDbContextAsync(cancellationToken);
 
-        var entity = await appDbContext.BusinessCards
-            .FirstOrDefaultAsync(businessCard => businessCard.Id == id, cancellationToken);
+            var entity = await appDbContext.BusinessCards
+                .FirstOrDefaultAsync(businessCard => businessCard.Id == id, cancellationToken);
 
-        if (entity is null)
+            if (entity is null)
+                return false;
+
+            appDbContext.BusinessCards.Remove(entity);
+            await appDbContext.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+        catch
+        {
+            await dialogService.ShowMessageAsync(AppStrings.Dialogs.UnableToConnectDatabase, Enums.DialogType.Error);
+            Process.GetCurrentProcess().Kill();
             return false;
-
-        appDbContext.BusinessCards.Remove(entity);
-        await appDbContext.SaveChangesAsync(cancellationToken);
-
-        return true;
+        }
     }
 }
